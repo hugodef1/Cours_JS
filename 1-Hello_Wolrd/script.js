f9439f7ff735bc1ddc8e7040f728c97bdc421721
hugo defaye <hugodef33@gmail.com>
2025-09-11 11:23:09 +0200

    init TP 1-6

---
+++ 1-Hello_Wolrd/script.js
@@ -0,0 +1,12 @@
+//Commentaire
+var mere = prompt("quel est le nom de la mere de lucas ?");
+if (mere == "" || mere == null){
+    console.log("La mere de lucas n a pas de nom");
+}else{
+    console.log("le prenom de la mere de lucas est "+mere);
+}
+
+let monPrenom;  /* Déclaration d'une variable */
+monPrenom = "Toto"; /* Affectation d'une variable */
+
+let nom = "Dupont"; /* Déclaration et affectation d'une variable */
---
+++ 2-Les_Variables/script.js
@@ -0,0 +1,33 @@
+//TP4
+let nomJour = "Jeudi";
+let numJour = 11;
+let nomMois = "Septembre"
+let isHiver = false;
+console.log(nomJour + numJour + nomMois + isHiver)
+//TP5
+let tabJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+let tabMois = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+console.log(tabJours[3]);
+console.log(tabMois[7]);
+let maDateV1 = {
+    "jour" : "Jeudi",
+    "mois" : "Septembre"
+}
+let maDateV2 = {};
+maDateV2.jour = "Jeudi";
+maDateV2.mois = "Septembre";
+console.log(maDateV1.jour + maDateV1.mois);
+console.log(maDateV2.jour + maDateV2.mois);
+//TP6
+let nbr1 = 3;
+let nbr2 = 7;
+let nbr3 = 8;
+let nbr4 = 21;
+console.log(nbr1 + nbr2);
+console.log(nbr4 - nbr3);
+nbr2=nbr1*nbr2;
+console.log(nbr2);
+console.log(nbr2+1);
+console.log(nbr4 / nbr1);
+console.log(nbr4 % nbr1);
+//TP7
