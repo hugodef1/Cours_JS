484de48e92a8bb3f3d7cfa958f76f1d25825064d
hugo defaye <hugodef33@gmail.com>
2026-01-29 09:21:04 +0100

    TP HP

--- 4-LesBoucles/script.js
+++ 4-LesBoucles/script.js
@@ -1,10 +1,12 @@
 //TP11
+//fait j! avec while 
 let i=0, j=0
 while(i<=100){
     j=j+i
     i++
 }
 console.log(j)
+//fait j! avec do while
 let x=prompt("Saisi un nombre"), y=0, z=0
 do{
     z=z+y
@@ -12,7 +14,8 @@ do{
 }while(y<=x)
 console.log(z)
 //TP12
+//affiche les elements d'un tableau avec for
 let t=[1,2,3,4,5,6,7,8]
-for (let e = 0; e<=t.length; e++){
+for (let e = 0; e<t.length; e++){
     console.log(t[e])
 }
